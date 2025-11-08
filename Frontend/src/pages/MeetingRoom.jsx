13aa0b16affabcea2f04207c8fd55017b0b694a1
priyankahotkar <160253899+priyankahotkar@users.noreply.github.com>
2025-11-08 13:40:44 +0530

    Nothing

--- Frontend/src/pages/MeetingRoom.jsx
+++ Frontend/src/pages/MeetingRoom.jsx
@@ -567,7 +567,6 @@ function MeetingRoom() {
               </div>
             ))}
           </div>
-          
         )}
         <div className="controls-left">
           <span className="meeting-id-badge">ID: {meetingId}</span>
